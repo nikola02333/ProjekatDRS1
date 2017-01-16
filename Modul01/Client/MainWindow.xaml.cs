09d91ab20bbff9ddfd3470081e99eeb5a3fc8a8d
nikola02333 <nikolaniki640@gmail.com>
2017-01-16 13:19:46 +0100

    to je to

---
+++ Modul01/Client/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Biblioteka.Data;
+using Biblioteka.Interface;
+using Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ChannelFactory<IFunkcije> factory = new ChannelFactory<IFunkcije>(
+            new NetTcpBinding(),
+            new EndpointAddress("net.tcp://localhost:4000/IFunkcije"));
+
+     
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+       
+
+        private void Logovanje(object sender, RoutedEventArgs e)
+        {
+            IFunkcije proxy = factory.CreateChannel();
+
+            string username = usernameTB.Text;
+            string pass = passwordPB.Password;
+       
+
+            Osoba zaposlen = new Osoba();
+            zaposlen.KorIme = username;
+            zaposlen.Lozinka = pass;
+          
+
+
+            switch (proxy.SlanjeLoga(username, pass))  // 0- direktor , 1-hr , 2- radnik , 3-PO , 4 sm
+            {
+                case 0:
+                         Direktor zaposleni = new Direktor(zaposlen);
+                    
+                    zaposleni.Owner = this;
+
+                    zaposleni.ShowDialog();
+                  
+                    break;
+                case 1:
+                    SektorHR hr = new SektorHR(zaposlen);
+                    hr.Owner = this;
+                    hr.ShowDialog();
+                    break;
+                case 2:
+                    VlasnikProizvoda vl = new VlasnikProizvoda(zaposlen);
+                    vl.Owner = this;
+                    vl.ShowDialog();
+                    break;
+                case 3:
+                    Radnik radnik = new Radnik(zaposlen);
+                    radnik.Owner = this;
+                    radnik.ShowDialog();
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:  
+                    Radnik radnik1 = new Radnik(zaposlen);
+                    radnik1.Owner = this;
+                    radnik1.buttonVodjeTima.Visibility = System.Windows.Visibility.Visible;
+                    radnik1.ShowDialog();
+                    break;
+
+            }
+            
+        }
+     }
+}
---
+++ Modul01/Client/VlasnikProizvoda.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Biblioteka.Access;
+using Biblioteka.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Server;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for VlasnikProizvoda.xaml
+    /// </summary>
+    public partial class VlasnikProizvoda : Window
+    {
+        ChannelFactory<ICompanyDB> factory = new ChannelFactory<ICompanyDB>(
+               new NetTcpBinding(),
+               new EndpointAddress("net.tcp://localhost:4000/ICompanyDB"));
+
+        Osoba z;
+        Osoba trenutni;
+        DateTime vremeDolaskaNaPosao;
+        public VlasnikProizvoda(Osoba zaposlen)
+        {
+            InitializeComponent();
+            
+            ICompanyDB proxy = factory.CreateChannel();
+            List<Osoba> privremena = proxy.Read();
+
+            foreach (Osoba o in privremena)
+            {
+                if (o.KorIme.Equals(zaposlen.KorIme))
+                {
+                    if (o.Lozinka.Equals(zaposlen.Lozinka))
+                    {
+                        this.z = o;
+                        break;
+                    }
+                }
+            }
+
+            this.trenutni = z;
+            trenutni.Prijavljen = true;
+            proxy.ReplaceAction(z, trenutni);
+
+            this.vremeDolaskaNaPosao = DateTime.Now;
+            //         dolazakNaPosao();                               
+
+            promenaLozinke();
+
+            
+        }
+        private void promenaLozinke()
+        {
+            DateTime trenutnovreme = DateTime.Now;
+            DateTime lozinka = trenutni.VremeLozinke;
+
+            int razlika = Math.Abs(trenutnovreme.Month - lozinka.Month);
+
+            if (razlika == 6)
+            {
+                IzmenaPodataka izmena = new IzmenaPodataka(trenutni, factory, 2);
+                izmena.ShowDialog();
+            }
+        }
+
+
+
+
+        private void KreirajProjekat(object sender, RoutedEventArgs e)
+        {
+
+            
+                Projekat proj = new Projekat(textBoxIme.Text, textBoxOpis.Text,
+                textBoxKriterijunm.Text, dataPickerPoc.SelectedDate.Value, dataPickerKraj.SelectedDate.Value
+                , textBoxKorisnicaPrica.Text, int.Parse(textBoxTezina.Text), textBoxZadaci.Text, Program.ID_Proj);
+            
+           
+
+            Program.ID_Proj++;
+            bool x= ubaciUbazuProjekat(proj);
+
+        }
+        public bool ubaciUbazuProjekat(Projekat proj)
+        {
+            ICompanyDB proxy = factory.CreateChannel();
+
+            if( proxy.AddActionProject(proj))
+            {
+                MessageBox.Show("Projekat uspesno sacuvan u bazi :D ");
+            }
+
+            return true;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            ICompanyDB proxy = factory.CreateChannel();
+
+            trenutni.Prijavljen = false;
+            proxy.ReplaceAction(z, trenutni);
+            this.Close();
+        }
+    }
+}
